fa938eb8fefdd432872906f60cc5940a9f61b707
Seohd99 <105189672+Seohd99@users.noreply.github.com>
2024-01-13 15:57:25 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 4296 ms, Memory: 56384 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,27 @@
+n, m = map(int,input().split())
+
+lst = []
+
+def find(x):
+    if lst[x] == x: return lst[x]
+    lst[x] = find(lst[x])
+    return lst[x]
+
+
+for i in range(n+1):
+    lst.append(i)
+
+SUM = 0
+pay = []
+for i in range(m):
+    pay.append(list(map(int,input().split())))
+
+pay.sort(key=lambda x : x[2])
+
+for i in pay:
+    x = find(i[0])
+    y = find(i[1])
+    if x != y:
+        SUM += i[2]
+        lst[y] = x
+print(SUM)
