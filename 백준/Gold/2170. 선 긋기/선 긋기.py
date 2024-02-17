84625f9f29551cf2849eeb871d8b552fdf3432df
Seohd99 <105189672+Seohd99@users.noreply.github.com>
2024-02-17 13:04:38 +0900

    [Gold V] Title: 선 긋기, Time: 4388 ms, Memory: 250028 KB -BaekjoonHub

--- 백준/Gold/2170. 선 긋기/선 긋기.py
+++ 백준/Gold/2170. 선 긋기/선 긋기.py
@@ -1,21 +1,26 @@
 import sys
-
-points = []
 n = int(input())
-for _ in range(n):
-    s, e = map(int, sys.stdin.readline().split())
-    points.append((s, 1))
-    points.append((e, -1))
 
-points.sort()
-result = 0
-check = 0
-s = 0
-for point in points:
-    # 선이 시작 +1, 선이 끝나면 -1을 해준다.
-    if check == 0:
-        s = point[0]
-    check += point[1]
-    if check == 0:
-        result += (point[0] - s)
-print(result)
+lst = []
+for i in range(n):
+    a, b = map(int,sys.stdin.readline().split())
+    lst.append((a,1))
+    lst.append((b,-1))
+
+lst.sort()
+sw = 0
+SUM = 0
+for i in lst:
+    if 1 == i[1]:
+        sw += 1
+        if sw == 1:
+            s = i[0]
+    else:
+        sw -= 1
+        if sw == 0:
+            SUM += (i[0] - s)
+print(SUM)
+
+
+
+
