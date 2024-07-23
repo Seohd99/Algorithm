2619a41a8566d33742f4e2e3756ccb3cdae8c3aa
Seohd99 <105189672+Seohd99@users.noreply.github.com>
2024-07-23 13:54:49 +0900

    [Silver I] Title: 동물원, Time: 116 ms, Memory: 115804 KB -BaekjoonHub

--- 백준/Silver/1309. 동물원/동물원.py
+++ 백준/Silver/1309. 동물원/동물원.py
@@ -1,12 +1,9 @@
 n = int(input())
 
-l = [[0,0] for i in range(n+1)]
-l[1] = [1,2]
+dp = [0,3,7]
+
 for i in range(2,n+1):
-    for j in range(2):
-        if j == 0:
-            l[i][j] = sum(l[i-1]) % 9901
-        else:
-            l[i][j] = l[i-1][0] * 2 + l[i-1][1] % 9901
-    
-print(sum(l[-1]) % 9901)  
+    x = (dp[i-1] + dp[i] * 2) % 9901
+    dp.append(x)
+
+print(dp[n])
