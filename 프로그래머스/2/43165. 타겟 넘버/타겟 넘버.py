c5c7d80e4983cac0de14993ccf8a90358ffdb476
Seohd99 <105189672+Seohd99@users.noreply.github.com>
2025-11-14 13:36:21 +0900

    [level 2] Title: 타겟 넘버, Time: 979.19 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.py
@@ -3,5 +3,21 @@ def dfs(i,x,numbers, target):
         return 1 if x == target else 0
     return dfs(i+1, x+numbers[i], numbers, target) + dfs(i+1, x-numbers[i], numbers, target)
 
+from collections import deque
+
 def solution(numbers, target):
-    return dfs(0,0,numbers,target)
+    answer = 0
+    q = deque()
+    
+    q.append([0,0])
+    
+    while q:
+        x,i = q.popleft()
+        if i == len(numbers):
+            if target == x:
+                answer += 1
+            continue
+        q.append([x + numbers[i],i + 1])
+        q.append([x - numbers[i],i + 1])
+    
+    return answer
